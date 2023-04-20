d274abb6e0657bfc18b9fad139dd0fd6abca16f9
jfrences11 <jfrences11@gmail.com>
2023-04-20 21:13:24 +0100

    add Transaction

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,22 +19,27 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    // 함수가 시작될 때 start transaction
+    // 함수가 문제 없이 실행되었다면 commit
+    // 문제가 발생했다면 rollback
+    @Transactional
     public void saveUser(UserCreateRequest request) {
         userRepository.save(new User(request.getName(), request.getAge()));
     }
-
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers() {
         List<User> users = userRepository.findAll();
         return users.stream().map(UserResponse::new).collect(Collectors.toList());
     }
-
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         User user = userRepository.findById(request.getId()).orElseThrow(IllegalArgumentException::new);
 
         user.updateName(request.getName());
-        userRepository.save(user);
+        // 같은 영속성 컨텍스트 안에서는 생략 가능
+        //userRepository.save(user);
     }
-
+    @Transactional
     public void deleteUser(String name) {
         User user = userRepository.findByName(name);
         if(user == null) {
